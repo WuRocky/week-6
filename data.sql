c7d07e51db4a92685375cc262347119b4900d10e
Rocky Wu <rockywu971@gmail.com>
2022-10-25 22:48:19 +0800

    2022/10/25 10:48

--- data.sql
+++ data.sql
@@ -38,7 +38,7 @@ CREATE TABLE `member` (
   `follower_count` int unsigned NOT NULL DEFAULT '0',
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES (1,'test2','test','test',52,'2022-10-18 09:03:36'),(2,'Rocky','rocky','rocky',10,'2022-10-18 09:03:43'),(3,'Leo','leo','leo',0,'2022-10-18 09:03:50'),(4,'John','john','john',25,'2022-10-18 09:03:56'),(5,'David','david','david',0,'2022-10-18 09:04:01'),(6,'Allen','Allen','Allen',0,'2022-10-25 10:30:44'),(7,'Cary','Cary','Cary',0,'2022-10-25 11:09:41'),(8,'哈哈','111','111',0,'2022-10-25 11:35:07'),(10,'chin','chin','chin',0,'2022-10-25 20:19:29'),(11,'Ava','Ava','ava',0,'2022-10-25 20:23:00');
+INSERT INTO `member` VALUES (1,'test2','test','test',52,'2022-10-18 09:03:36'),(2,'Rocky','rocky','rocky',10,'2022-10-18 09:03:43'),(3,'Leo','leo','leo',0,'2022-10-18 09:03:50'),(4,'John','john','john',25,'2022-10-18 09:03:56'),(5,'David','david','david',0,'2022-10-18 09:04:01'),(6,'Allen','Allen','Allen',0,'2022-10-25 10:30:44'),(7,'Cary','Cary','Cary',0,'2022-10-25 11:09:41'),(8,'哈哈','111','111',0,'2022-10-25 11:35:07'),(10,'chin','chin','chin',0,'2022-10-25 20:19:29'),(11,'Ava','Ava','ava',0,'2022-10-25 20:23:00'),(12,'Ben','Ben','Ben',0,'2022-10-25 21:29:42');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,7 +67,7 @@ CREATE TABLE `message` (
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`),
   CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +76,7 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,1,'好喜歡Wehelp喔',70,'2022-10-18 09:12:22'),(2,1,'大家加油!!!',45,'2022-10-18 09:12:28'),(3,1,'每天都要學習!!!',50,'2022-10-18 09:12:33'),(4,2,'加油喔',3,'2022-10-18 09:12:38'),(5,2,'腳傷快點好~',2,'2022-10-18 09:12:45'),(6,2,'辛苦了',1,'2022-10-18 09:12:52'),(7,3,'哈哈',0,'2022-10-18 09:13:01'),(8,3,'大家好',0,'2022-10-18 09:13:10'),(9,4,'天氣好差',0,'2022-10-18 09:13:15'),(10,5,'希望明天有太陽',10,'2022-10-18 09:13:22'),(11,5,'希望明天不要下雨',20,'2022-10-18 09:13:31'),(23,1,'測試',0,'2022-10-25 16:35:23'),(24,1,'哈哈',0,'2022-10-25 20:26:09');
+INSERT INTO `message` VALUES (1,1,'好喜歡Wehelp喔',70,'2022-10-18 09:12:22'),(2,1,'大家加油!!!',45,'2022-10-18 09:12:28'),(3,1,'每天都要學習!!!',50,'2022-10-18 09:12:33'),(4,2,'加油喔',3,'2022-10-18 09:12:38'),(5,2,'腳傷快點好~',2,'2022-10-18 09:12:45'),(6,2,'辛苦了',1,'2022-10-18 09:12:52'),(7,3,'哈哈',0,'2022-10-18 09:13:01'),(8,3,'大家好',0,'2022-10-18 09:13:10'),(9,4,'天氣好差',0,'2022-10-18 09:13:15'),(10,5,'希望明天有太陽',10,'2022-10-18 09:13:22'),(11,5,'希望明天不要下雨',20,'2022-10-18 09:13:31'),(23,1,'測試',0,'2022-10-25 16:35:23'),(24,1,'哈哈',0,'2022-10-25 20:26:09'),(25,2,'測試',0,'2022-10-25 22:45:56');
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-25 20:37:18
+-- Dump completed on 2022-10-25 22:47:34
